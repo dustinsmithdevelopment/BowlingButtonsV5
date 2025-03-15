63997604a48f06817bb0a88eaf3b88c2e81109a9
Dustin Smith <dustinsmithdevelopment@gmail.com>
2025-03-15 13:54:38 -0500

    Adding button flash and sound

--- BowlingButtonsV2.ts
+++ BowlingButtonsV2.ts
@@ -1,5 +1,5 @@
-import {CodeBlockEvent, Component, Player, PropTypes} from "horizon/core";
-import {Pressable, Text, UIComponent, UINode, View} from "horizon/ui";
+import {AudioGizmo, CodeBlockEvent, Component, Player, PropTypes} from "horizon/core";
+import {Binding, Pressable, Text, UIComponent, UINode, View} from "horizon/ui";
 
 const START_EVENT_NAME = "startGame";
 const RESET_EVENT_NAME = "resetGame";
@@ -26,6 +26,12 @@ const skipPlayerEvent = new CodeBlockEvent<[player: Player]>(SKIP_PLAYER_EVENT_N
 
 const buttonStyling = {width: 100, height: 100};
 
+const startButtonColor = new Binding("green");
+const resetButtonColor = new Binding("red");
+const ballGlitchButtonColor = new Binding("yellow");
+const joinGameButtonColor = new Binding("blue");
+const skipPlayerButtonColor = new Binding("purple");
+
 function BorderBox(button: UINode, borderRadius: number|undefined = undefined): UINode{
   if (borderRadius === undefined){
     return View({
@@ -44,54 +50,56 @@ class BowlingButtons extends UIComponent<typeof BowlingButtons> {
   panelHeight = 300;
   panelWidth = 600;
   static propsDefinition = {
-    manager: {type: PropTypes.Entity}
+    manager: {type: PropTypes.Entity},
+    SFX: {type: PropTypes.Entity},
   };
+  private soundGizmo: AudioGizmo | undefined;
   initializeUI(): UINode {
     return View({
       children: [
         View({children: [
             View({style: {height: "25%"}}),
             BorderBox(Pressable({onClick: (player) => {
-                this.sendCodeBlockEvent(this.props.manager!, startEvent, player)
+                this.handleButtonPress(player, startEvent, startButtonColor);
               } ,children: [
                 Text({text: "Start",
                   style: {color: "white", height: '100%', textAlignVertical: 'center', textAlign: 'center'}})]
-              , style: {backgroundColor: "green", ...buttonStyling}})),
+              , style: {backgroundColor: startButtonColor, ...buttonStyling}})),
           ]}),
         View({style: {width: START_SEPARATION}}),
         View({children: [
             View({style: {height: BUTTON_SPACING}}),
             BorderBox(Pressable({onClick: (player) => {
-                this.sendCodeBlockEvent(this.props.manager!, resetEvent, player)
+                this.handleButtonPress(player, resetEvent, resetButtonColor);
               } ,children: [
                 Text({text: "Reset",
                   style: {color: "white", height: '100%', textAlignVertical: 'center', textAlign: 'center'}})],
-              style: {backgroundColor: "red", ...buttonStyling, borderRadius: BUTTON_BORDER_RADIUS}}), BUTTON_BORDER_RADIUS),
+              style: {backgroundColor: resetButtonColor, ...buttonStyling, borderRadius: BUTTON_BORDER_RADIUS}}), BUTTON_BORDER_RADIUS),
             View({style: {height: BUTTON_SPACING}}),
             BorderBox(Pressable({onClick: (player) => {
-                this.sendCodeBlockEvent(this.props.manager!, ballGlitchEvent, player)
+                this.handleButtonPress(player, ballGlitchEvent, ballGlitchButtonColor);
               } ,children: [
                 Text({text: "Ball Glitched",
                   style: {color: "black", height: '100%', textAlignVertical: 'center', textAlign: 'center'}})],
-              style: {backgroundColor: "yellow", ...buttonStyling, borderRadius: BUTTON_BORDER_RADIUS}}),BUTTON_BORDER_RADIUS),
+              style: {backgroundColor: ballGlitchButtonColor, ...buttonStyling, borderRadius: BUTTON_BORDER_RADIUS}}),BUTTON_BORDER_RADIUS),
             View({style: {height: BUTTON_SPACING}})],
           style:{display:"flex",flexDirection:"column",justifyContent:"center"}}),
         View({style: {width: BUTTON_SPACING}}),
         View({children: [
             View({style: {height: BUTTON_SPACING}}),
             BorderBox(Pressable({onClick: (player) => {
-                this.sendCodeBlockEvent(this.props.manager!, joinEvent, player)
+                this.handleButtonPress(player, joinEvent, joinGameButtonColor);
               } ,children: [
                 Text({text: "Join",
                   style: {color: "white", height: '100%', textAlignVertical: 'center', textAlign: 'center'}})],
-              style: {backgroundColor: "blue", ...buttonStyling, borderRadius: BUTTON_BORDER_RADIUS}}),BUTTON_BORDER_RADIUS),
+              style: {backgroundColor: joinGameButtonColor, ...buttonStyling, borderRadius: BUTTON_BORDER_RADIUS}}),BUTTON_BORDER_RADIUS),
             View({style: {height: BUTTON_SPACING}}),
             BorderBox(Pressable({onClick: (player) => {
-                this.sendCodeBlockEvent(this.props.manager!, skipPlayerEvent, player)
+                this.handleButtonPress(player, skipPlayerEvent, skipPlayerButtonColor);
               } ,children: [
                 Text({text: "Skip Turn",
                   style: {height: '100%', textAlignVertical: 'center', textAlign: 'center'}})],
-              style: {backgroundColor: "purple", ...buttonStyling, borderRadius: BUTTON_BORDER_RADIUS}}),BUTTON_BORDER_RADIUS),
+              style: {backgroundColor: skipPlayerButtonColor, ...buttonStyling, borderRadius: BUTTON_BORDER_RADIUS}}),BUTTON_BORDER_RADIUS),
             View({style: {height: BUTTON_SPACING}})],
           style:{display:"flex",flexDirection:"column",justifyContent:"center"}}),
         View({style: {width: BUTTON_SPACING}})
@@ -100,7 +108,15 @@ class BowlingButtons extends UIComponent<typeof BowlingButtons> {
   }
 
   start() {
-
+    if (this.props.SFX){
+      this.soundGizmo = this.props.SFX.as(AudioGizmo);
+    }
+  }
+  private handleButtonPress(player: Player, event: CodeBlockEvent<[player: Player]>, buttonBinding: Binding<string>) {
+    this.sendCodeBlockEvent(this.props.manager!, event, player);
+    buttonBinding.set("white");
+    this.soundGizmo?.play();
+    this.async.setTimeout(()=>{buttonBinding.reset()},100);
   }
 }
 Component.register(BowlingButtons);
